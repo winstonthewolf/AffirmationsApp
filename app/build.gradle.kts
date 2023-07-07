8a664915f54f040ed221fac54fd78dba8d132f7a
Hugo Reyes <31859922+winstonthewolf@users.noreply.github.com>
2023-07-07 05:47:53 -0700

    Finished App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,11 +57,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.7"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+
 }
 
 dependencies {
